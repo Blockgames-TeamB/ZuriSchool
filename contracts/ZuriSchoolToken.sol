c55afc37fed3414b7c9220874fff7c77cc2cf543
sanCrystal <anyanwu.amanzearthur@gmail.com>
2022-05-03 13:11:31 +0100

    retouched tokenContract

--- contracts/ZuriSchoolToken.sol
+++ contracts/ZuriSchoolToken.sol
@@ -1,54 +1,4 @@
-// //SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.10;
-
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-// /**
-// * @author TeamB - Blockgames Internship 2022
-// * @title ZuriSchoolToken minting contract
-// */
-// contract ZuriSchoolToken is ERC20 {
-//     address public chairman;
-//     constructor() ERC20("ZuriSchoolToken", "ZST") {
-//         chairman = msg.sender;
-//    _mint(msg.sender,40*1e18);
-//     }
-
-//  mapping(address => bool) public teacher;
-
-//     modifier onlyAccess() {
-
-//         /// @notice check that sender is the chairman
-//         require(msg.sender == chairman || teacher[msg.sender] == true, 
-//         "Access granted to only the chairman or teacher");
-//         _;
-//     }
-
-//     /** 
-//     * @notice mints specified amount of tokens to an address.
-//     * @dev callable only by owner of contract
-//     */
-//     function mint(address _to, uint256 _amount) public onlyAccess{
-//         _mint(_to, _amount);
-//     }
-
-//     function mintToStakeholder(uint256 _amountOftoken, string calldata _role, address[] calldata _address) external  {
-        
-//         /// @notice loop through the list of students and upload
-//         require(
-//             _address.length >0,
-//             "Upload array of addresses"
-//         );
-        
-//         for (uint i = 0; i < _address.length; i++) {
-//             //mint 5 tokens to students,10 tokens to teachers and 20 to directors
-//             _mint(_address[i],_amountOftoken*1e18);
-
-//         }
-       
-//     }
-// }
-
+//SPDX-License-Identifier: MIT
 
 /**
 * @author TeamB - Blockgames Internship 2022
@@ -58,9 +8,8 @@ pragma solidity ^0.8.10;
 
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
- contract ZuriSchoolToken is ERC20, Ownable {
+ contract ZuriSchoolToken is ERC20{
     address public chairman;
     /// ------------------------------------- MAPPING ------------------------------------------ ///
     mapping(address => bool) public teacher;
